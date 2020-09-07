0832ba73d46012d7ac8470e9d8e6670d5c74f5ed
Vijay Stroup <vijaystroup@gmail.com>
2020-09-06 22:12:12 -0400

    error checking

--- get_data.py
+++ get_data.py
@@ -20,29 +20,38 @@ def transform_table(table):
     """take in the souped table and transform it to a dictionary with
     the key being the date and the values being the pdf links."""
 
-    rows = table.findChildren('tr')
-    rows.pop(0) # remove info row
+    try:
+        rows = table.findChildren('tr')
+        rows.pop(0) # remove info row
+    except Exception as e:
+        return f'error finding "tr" children: {e}'
 
     exams = {}
     for row in rows[::2]:
-        exam_info = row.findChildren('td')
+        try:
+            exam_info = row.findChildren('td')
+        except Exception as e:
+            return f'error finding "td" children: {e}'
 
-        # make dict key
-        key = exam_info[0].text.split(' ')
-        key.pop(1)
-        key = (' ').join(key)
+        try:
+            # make dict key
+            key = exam_info[0].text.split(' ')
+            key.pop(1)
+            key = (' ').join(key)
 
-        # get questions pdf url
-        question = exam_info[1].find(href=True)['href']
-        question = url + question
+            # get questions pdf url
+            question = exam_info[1].find(href=True)['href']
+            question = url + question
 
-        # get answers pdf url
-        answer = exam_info[2].find(href=True)['href']
-        answer = url + answer
+            # get answers pdf url
+            answer = exam_info[2].find(href=True)['href']
+            answer = url + answer
 
-        # get info pdf url
-        info = exam_info[3].find(href=True)['href']
-        info = url + info
+            # get info pdf url
+            info = exam_info[3].find(href=True)['href']
+            info = url + info
+        except Exception as e:
+            return f'error getting dict attributes: {e}'
 
         # append exam to exams
         exams[key] = {
