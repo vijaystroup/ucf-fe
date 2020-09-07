25352b19f95e126b71572f8abbe7fa1d0dad5c1b
Vijay Stroup <vijaystroup@gmail.com>
2020-09-06 21:40:25 -0400

    looping through table rows

--- get_data.py
+++ get_data.py
@@ -4,6 +4,9 @@ import requests
 
 
 def get_page(url):
+    """request the url for the past exams and return the soup for
+    that request. if unable to get the page, return error message."""
+
     req = requests.get(url)
     if req.status_code == 200:
         try:
@@ -13,8 +16,23 @@ def get_page(url):
     return f'error requesting: {url}\nstatus: {req.status_code}'
 
 
+def transform_table(table):
+    """take in the souped table and transform it to a dictionary with
+    the key being the date and the values being the pdf links."""
+
+    rows = table.findChildren('tr')
+    rows.pop(0) # remove info row
+
+    exams = {}
+    for row in rows[::2]:
+        print(row.findChildren('td'))
+        print()
+
+
 if __name__ == '__main__':
     url = 'http://www.cs.ucf.edu/registration/exm/'
+
     page = get_page(url)
+
     table = page.find_all('table')[1]
-    print(table)
+    transform_table(table)
