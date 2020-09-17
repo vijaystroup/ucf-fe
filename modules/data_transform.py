b9978b0aa916c8c8690b9b6a74f0fed0db8f0906
Vijay Stroup <vijaystroup@gmail.com>
2020-09-17 12:45:58 -0400

    check to make sure questions for a test have not already been generated

--- modules/data_transform.py
+++ modules/data_transform.py
@@ -11,9 +11,10 @@ path_question = path + '/question'
 raw_q = os.listdir(path_raw)
 created_q = os.listdir(path_question)
 
-# FE-Aug20-12
+
 def pre_existing_q(search):
     """check to see if we have already created questions for a specific test"""
+
     search = search.split('-')[1].split('.')[0]
     for q in created_q:
         if search in q:
@@ -22,14 +23,11 @@ def pre_existing_q(search):
 
 
 def make_question():
-    for i in range(len(raw_q)):
-        pdf = PdfFileReader(open(raw_dir + raw_q[i], 'rb'))
+    """make small question pdfs using metadata"""
+
+    for q in raw_q:
+        if not pre_existing_q(q):
+            pdf = PdfFileReader(open(raw_dir + raw_q[i], 'rb'))
 
 
-# print(md.questions[raw_q[0]])
-
-# print(created_q)
-# print()
-# print(raw_q)
-pre_existing_q(raw_q[0])
-# make_question()
+make_question()
