a009873c45c9a2e5e1cc8cc13b0c5e1b7d936162
Vijay Stroup <vijaystroup@gmail.com>
2020-09-16 22:00:29 -0400

    regex searching for number

--- data_transform.py
+++ data_transform.py
@@ -1,10 +1,20 @@
 import os
+import re
 from PyPDF2 import PdfFileReader
 
-path = os.path.abspath(os.path.dirname(__name__)) + '/'
-question_dir = path + 'data/question/'
-answer_dir = path + 'data/answer/'
-info_dir = path + 'data/info/'
+path = os.path.abspath(os.path.dirname(__name__)) + '/static/'
+raw_dir = path + 'raw_tests/'
+question_dir = path + 'question/'
+answer_dir = path + 'answer/'
+info_dir = path + 'info/'
 
-input1 = PdfFileReader(open(question_dir + 'FE-Aug20.pdf', 'rb'))
-print(input1.getPage(1).extractText())
+input1 = PdfFileReader(open(raw_dir + 'FE-Aug20.pdf', 'rb'))
+input2 = PdfFileReader(open(raw_dir + 'FE-Aug15.pdf', 'rb'))
+input3 = PdfFileReader(open(raw_dir + 'FE-May18.pdf', 'rb'))
+text = input3.getPage(1).extractText().splitlines()
+print(text)
+rec = re.compile('([1-9])\)')
+for i in text:
+    res = re.search(rec, i)
+    if res is not None:
+        print(res.group(0))
