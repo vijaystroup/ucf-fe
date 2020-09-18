0ed858ee55a6528d9c6fefdd74b8497d5e17c412
Vijay Stroup <vijaystroup@gmail.com>
2020-09-18 18:12:33 -0400

    more css

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ app = Flask(__name__)
 def index():
     questions = os.listdir(os.path.abspath(os.path.dirname(__name__)) + '/static' + '/question')
     question = random.choice(questions)
-    question_info = 'Info-' + question.split('-')[1] + '.pdf'
+    question_info = 'Info-' + question.split('-')[1]
+
     return render_template('index.html', question=question, question_info=question_info)
 
 
