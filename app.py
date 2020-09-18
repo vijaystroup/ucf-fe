ee4647302312455d3536cf384a06ae0faf42e976
Vijay Stroup <vijaystroup@gmail.com>
2020-09-18 13:37:05 -0400

    html and css skeleton

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
+import os
+import random
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    questions = os.listdir(os.path.abspath(os.path.dirname(__name__)) + '/static' + '/question')
+    return render_template('index.html', question=random.choice(questions))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
