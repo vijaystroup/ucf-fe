17a8f5926ee6b929bc49e1b9e131ad61844b2de8
Vijay Stroup <vijaystroup@gmail.com>
2020-09-18 19:56:37 -0400

    timer with positive numbers

--- static/index.js
+++ static/index.js
@@ -1,3 +1,39 @@
+const playBtn = document.getElementById('start')
+const timer = document.getElementById('timer')
+
 function switchTheme() {
   document.body.classList.toggle('dark')
+}
+
+function startTimer() {
+  timer.innerHTML = -0.50.toFixed(2)
+
+  setInterval(function() {
+    let time = +(timer.innerHTML)
+
+    // if (time % 1 == 0 || time % 1 == 59) {
+    //   if (time > 0) {
+    //     time = time - .40
+    //   }
+    //   else if (time < 0) {
+    //     time = time - .40
+    //   }
+    // }
+
+    if (time > 1) {
+      if (time % 1 == 0) {
+        time = time - .40
+      }
+    }
+    else if (time < 1) {
+      if (Math.abs(time) % 1 == 59) {
+        time = time - .40
+      }
+    }
+
+
+
+
+    timer.innerHTML = (Math.round((time - .01) * 100) / 100).toFixed(2)
+  }, 1000)
 }
