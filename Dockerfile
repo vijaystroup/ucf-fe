fcea9f0e2fde628f481b200d06c57c95be59e908
Vijay Stroup <vijaystroup@gmail.com>
2020-10-20 17:53:30 -0400

    image name fix

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM tiangolo/uwsgi-nginx-flask:python3.8-alpine
 
 WORKDIR /app
 COPY . .
-RUN apk add libxml2 libxslt
+RUN apk add libxml2 && apk add libxslt
 RUN pip install -r requirements.txt
 RUN python3 modules/get_data.py
--- deploy.sh
+++ deploy.sh
@@ -4,12 +4,12 @@
 
 # build image
 printf "\n\nBuilding image\n==============\n"
-docker rmi -f ucf-fe
+docker rmi -f ucf_fe
 docker rmi $(docker images | grep "^<none>" | awk "{print $3}")
-docker build -t ucf-fe .
+docker build -t ucf_fe .
 sleep 3 # sleep for 3 seconds to make sure image is done completely
 
 # deploy stack
 printf "\n\nDeploying stack\n===============\n"
-docker stack rm ucf-fe
-docker stack deploy -c docker-compose.traefik.yml ucf-fe
+docker stack rm ucf_fe
+docker stack deploy -c docker-compose.traefik.yml ucf_fe
