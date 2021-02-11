8d4b17c78aac1bd263139c454afebdf0909706d5
Vijay Stroup <vijaystroup@gmail.com>
2021-02-11 16:35:13 -0500

    temp fix till info pdf is given for jan21

--- modules/get_data.py
+++ modules/get_data.py
@@ -28,6 +28,8 @@ def transform_table(table):
         return (-1, f'error finding "tr" children')
     rows.pop(0) # remove info row
 
+    rows = rows[2:] # temp delete jan21 till info section is added
+
     exams = {}
     for row in rows[::2]:
         exam_info = row.findChildren('td')
