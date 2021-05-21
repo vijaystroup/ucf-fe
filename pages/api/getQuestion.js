6aa6fe9c18730b8fcea2adc4fac584a1e290a28f
Vijay Stroup <vijaystroup@gmail.com>
2021-05-21 10:12:49 -0400

    vercel dirname test

--- pages/api/getQuestion.js
+++ pages/api/getQuestion.js
@@ -6,6 +6,7 @@ const { serverRuntimeConfig } = getConfig()
 export default async function handler(req, res) {
   const data = {}
   const files = readdirSync(join(serverRuntimeConfig.PROJECT_ROOT, './public/question'))
+  console.log(__dirname)
 
   const file = files[Math.floor(Math.random() * files.length)]
   const n = file.split('.')[0].split('-')[2]
