6c405d3e3da2391385691fce729c5f3627e30ed6
Vijay Stroup <vijaystroup@gmail.com>
2021-05-20 20:33:01 -0400

    change default value

--- components/code.jsx
+++ components/code.jsx
@@ -1,11 +1,14 @@
 import Editor from '@monaco-editor/react'
 
-export default function Code({ theme }) {
+export default function Code({ theme, statistics }) {
   return (
     <>
       <Editor
         defaultLanguage='cpp'
-        defaultValue='// Your solution here'
+        defaultValue={
+          `// formula sheet: https://ucffe.vijaystroup.com/misc/FE-FormulaSheet.pdf${'\n'}` + 
+          `// statistics: ${statistics}${'\n\n'}`
+        }
         theme={`vs-${theme}`}
       />
     </>
