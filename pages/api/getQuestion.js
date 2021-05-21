e201359147cbefd1f9de62574b24ba685c5d94bd
Vijay Stroup <vijaystroup@gmail.com>
2021-05-21 10:08:20 -0400

    set basedir

--- pages/api/getQuestion.js
+++ pages/api/getQuestion.js
@@ -1,9 +1,11 @@
 import { readdirSync } from 'fs'
 import { join } from 'path'
+import getConfig from 'next/config'
+const { serverRuntimeConfig } = getConfig()
 
 export default async function handler(req, res) {
   const data = {}
-  const files = readdirSync(join(__dirname, 'public/question'))
+  const files = readdirSync(join(serverRuntimeConfig.PROJECT_ROOT, './public/question'))
 
   const file = files[Math.floor(Math.random() * files.length)]
   const n = file.split('.')[0].split('-')[2]
