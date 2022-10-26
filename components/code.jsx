bd5e062e0f27d342720f13491b4d7e63afea7989
zenith110 <thequake@live.com>
2022-10-26 19:00:44 -0400

    Changed encoding to lf

--- components/code.jsx
+++ components/code.jsx
@@ -1,40 +1,42 @@
-import { useState } from 'react'
-import Editor from '@monaco-editor/react'
-import style from '../styles/code.module.scss'
+import { useState } from "react";
+import Editor from "@monaco-editor/react";
+import style from "../styles/code.module.scss";
 
 export default function Code({ theme, comments }) {
-  const [code, setCode] = useState(comments)
-  const [output, setOutput] = useState('')
+  const [code, setCode] = useState(comments);
+  const [output, setOutput] = useState("");
 
   async function run() {
-    const res = await fetch('https://emkc.org/api/v2/piston/execute', {
-      method: 'POST',
+    const res = await fetch("https://emkc.org/api/v2/piston/execute", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        language: 'c',
-        version: '10.2.0',
-        files: [{
-          content: code
-        }]
-      })
-    })
+        language: "c",
+        version: "10.2.0",
+        files: [
+          {
+            content: code,
+          },
+        ],
+      }),
+    });
 
     if (!res.ok) {
-      setOutput('Too many requests. Please try again later.')
-      return
+      setOutput("Too many requests. Please try again later.");
+      return;
     }
 
-    const data = await res.json()
-    setOutput(data.run.output)
+    const data = await res.json();
+    setOutput(data.run.output);
   }
 
   return (
     <div className={style.code}>
       <div className={style.editorWrapper1}>
         <Editor
-          defaultLanguage='cpp'
+          defaultLanguage="cpp"
           theme={`vs-${theme}`}
           value={code}
           onChange={(e) => setCode(e)}
@@ -42,16 +44,14 @@ export default function Code({ theme, comments }) {
       </div>
 
       <div className={style.runWrapper}>
-        <button className={style.run} onClick={run}>Run</button>
+        <button className={style.run} onClick={run}>
+          Run
+        </button>
       </div>
 
       <div className={style.editorWrapper2}>
-        <Editor
-          defaultLanguage='txt'
-          theme={`vs-${theme}`}
-          value={output}
-        />
+        <Editor defaultLanguage="txt" theme={`vs-${theme}`} value={output} />
       </div>
     </div>
-  )
+  );
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,43 +1,48 @@
-import Nav from '../components/nav'
-import Pdf from '../components/pdf'
-import Code from '../components/code'
-import style from '../styles/home.module.scss'
-import Footer from '../components/footer'
-import { useEffect, useState } from 'react'
-import getQuestion from '../lib/getQuestion'
+import Nav from "../components/nav";
+import Pdf from "../components/pdf";
+import Code from "../components/code";
+import style from "../styles/home.module.scss";
+import Footer from "../components/footer";
+import { useEffect, useState } from "react";
+import getQuestion from "../lib/getQuestion";
 
 export default function Home() {
-  const [theme, setTheme] = useState(0)
+  const [theme, setTheme] = useState(0);
   const [pdf, setPdf] = useState({
-    question: '/misc/instructions.pdf',
-    answer: '/misc/instructions.pdf',
-    info: 'N/A'
-  })
-  const [comments, setComments] = useState((
-    `// formula sheet: https://ucffe.vijaystroup.com/misc/FE-FormulaSheet.pdf${'\n'}` +
-    `// statistics: ${pdf['info']}${'\n\n'}` +
-    'int main(void) {\n    printf("Charge On!");\n    return 0;\n}\n'
-  ))
+    question: "/misc/instructions.pdf",
+    answer: "/misc/instructions.pdf",
+    info: "N/A",
+  });
+  const [comments, setComments] = useState(
+    `// formula sheet: https://ucffe.vijaystroup.com/misc/FE-FormulaSheet.pdf${"\n"}` +
+      `// statistics: ${pdf["info"]}${"\n\n"}` +
+      'int main(void) {\n    printf("Charge On!");\n    return 0;\n}\n'
+  );
 
   useEffect(async () => {
-    setPdf(await getQuestion())
+    setPdf(await getQuestion());
 
-    const resizer = document.getElementById(style.bar)
-    const pdfElement = document.getElementById('pdf')
+    const resizer = document.getElementById(style.bar);
+    const pdfElement = document.getElementById("pdf");
     resizer.onmousedown = () => {
-      pdfElement.style.zIndex = -1
-      resizer.parentNode.onmousemove = ev => {
+      pdfElement.style.zIndex = -1;
+      resizer.parentNode.onmousemove = (ev) => {
         if (window.innerWidth > 700) {
-          resizer.previousElementSibling.style.width = ev.clientX - resizer.offsetWidth/2 + 'px'
-          resizer.nextElementSibling.style.width = resizer.parentNode.offsetWidth - ev.clientX - resizer.offsetWidth/2 + 'px'
+          resizer.previousElementSibling.style.width =
+            ev.clientX - resizer.offsetWidth / 2 + "px";
+          resizer.nextElementSibling.style.width =
+            resizer.parentNode.offsetWidth -
+            ev.clientX -
+            resizer.offsetWidth / 2 +
+            "px";
         }
-      }
-    }
+      };
+    };
     resizer.onmouseup = () => {
-      resizer.parentNode.onmousemove = undefined
-      pdfElement.style.zIndex = 1
-    }
-  }, [])
+      resizer.parentNode.onmousemove = undefined;
+      pdfElement.style.zIndex = 1;
+    };
+  }, []);
 
   return (
     <>
@@ -47,13 +52,13 @@ export default function Home() {
           <section className={style.left}>
             <Pdf />
           </section>
-          <div className='resizer' id={style.bar}></div>
+          <div className="resizer" id={style.bar}></div>
           <section className={style.right}>
-            <Code theme={theme ? 'light' : 'dark'} comments={comments} />
+            <Code theme={theme ? "light" : "dark"} comments={comments} />
           </section>
         </main>
         <Footer setTheme={setTheme} />
       </div>
     </>
-  )
+  );
 }
